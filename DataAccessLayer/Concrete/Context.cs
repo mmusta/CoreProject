d75d9c6cbb6536873720c66384c2c4ffc053e00a
Mücahit Mustafa Usta <mmustafausta@yahoo.com>
2023-11-06 02:05:44 +0300

    Update Context.cs

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,17 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Concrete
 {
-	internal class Context
+	public class Context : DbContext
 	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=DESKTOP-9OEC3GE\\TEW_SQLEXPRESS;database=CoreProjectDb; Trusted_Connection=SSPI;Encrypt=false;TrustServerCertificate=true");
+		}
+		public DbSet<About> Abouts {  get; set; }
+		public DbSet<Blog> Blogs { get; set; }
+		public DbSet <Category> Categories { get; set;}
+		public DbSet <Comment> Comments { get; set; }
+		public DbSet <Contact> Contacts { get; set; }
+		public DbSet <Writer> Writers { get; set; }
 	}
 }
