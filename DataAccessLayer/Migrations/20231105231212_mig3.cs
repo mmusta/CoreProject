ff6f987facb4e4d576586c0c0eaf9a5126704649
Mücahit Mustafa Usta <mmustafausta@yahoo.com>
2023-11-06 02:13:04 +0300

    Create 20231105231212_mig3.cs

---
+++ DataAccessLayer/Migrations/20231105231212_mig3.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BlogId",
+                table: "Blogs",
+                newName: "BlogID");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "CommentStatus",
+                table: "Comments",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BlogID",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_BlogID",
+                table: "Comments",
+                column: "BlogID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Blogs_BlogID",
+                table: "Comments",
+                column: "BlogID",
+                principalTable: "Blogs",
+                principalColumn: "BlogID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Blogs_BlogID",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_BlogID",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "BlogID",
+                table: "Comments");
+
+            migrationBuilder.RenameColumn(
+                name: "BlogID",
+                table: "Blogs",
+                newName: "BlogId");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "CommentStatus",
+                table: "Comments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+    }
+}
