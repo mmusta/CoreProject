8b9772c256236368a1167703a94a29ba11e74b85
Mücahit Mustafa Usta <mmustafausta@yahoo.com>
2023-11-06 02:13:11 +0300

    Update Blog.cs

--- EntityLayer/Concrete/Blog.cs
+++ EntityLayer/Concrete/Blog.cs
@@ -10,13 +10,17 @@ namespace EntityLayer.Concrete
 	public class Blog
 	{
 		[Key]
-		public int BlogId { get; set; }
+		public int BlogID { get; set; }
 		public string? BlogTitle { get; set; }
 		public string? BlogContent { get;set; }
 		public string? BlogThumbnailImage { get; set; }
 		public string? BlogImage {  get; set; }
 		public DateTime BlogCreatDate { get; set; }
 		public bool BlogStatus { get; set; }
+		public int CategoryID { get; set ; }
+		public Category? Category { get; set; }
+
+		public List<Comment>? Comments { get; set; }
 
 	}
 }
